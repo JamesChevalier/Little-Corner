3f7d68b10fc105ee27c8cf3ee03bd2666054447d
James Chevalier <jchevalier@gmail.com>
2011-06-01 15:57:13 -0400

    Cleaned up the Bookmarks page

--- application/views/bookmarks.php
+++ application/views/bookmarks.php
@@ -3,7 +3,7 @@
 		<h1>Bookmarks</h1>
 		<?php
 		for($i=0;$i<count($posts);$i++){
-			echo "<div id=\"post\"><h2><a href=\"".$posts[$i]['link']."\" target=\"_blank\">".$posts[$i]['title']."</a></h2>\nPosted ".$this->myfunctions->relativeTime(strtotime($posts[$i]['pubDate']))."<br>".utf8_decode($posts[$i]['content'])."</div>";
+			echo "<div id=\"post\"><h2><a href=\"".$posts[$i]['link']."\" target=\"_blank\">".$posts[$i]['title']."</a></h2>\nPosted ".$this->myfunctions->relativeTime(strtotime($posts[$i]['pubDate']))."</div>";
 		}
 		?>
 		<a href="<?php echo $this->config->item('delicious_url'); ?>" target="_blank"><?php echo $this->config->item('my_name'); ?>'s Bookmarks</a>
