ec230c65d4315d3d75e53db3743d5bc32b499b5e
James Chevalier <jchevalier@gmail.com>
2012-12-24 12:42:32 -0500

    Minor code formatting

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,6 +1,7 @@
 class TwitterController < ApplicationController
 
   def index
+
     Twitter.configure do |config|
       config.consumer_key = TWITTER_CONSUMER_KEY
       config.consumer_secret = TWITTER_CONSUMER_SECRET
@@ -10,6 +11,7 @@ class TwitterController < ApplicationController
 
     @timeline = Twitter.user_timeline(TWITTER_USER_NAME)
     @favorites = Twitter.favorites(TWITTER_USER_NAME)
+
   end
 
 end
