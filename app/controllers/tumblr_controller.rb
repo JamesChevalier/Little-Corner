44b2617f4ee6a8bc9612252a6d89e3a538a1f47d
James Chevalier <jchevalier@gmail.com>
2013-01-01 19:09:30 -0500

    complete switch to simple-rss
    I don't care much for this implementation … I'll probably switch back to Feedzirra after it's updated to be compatible with Rails 3.2

--- app/controllers/tumblr_controller.rb
+++ app/controllers/tumblr_controller.rb
@@ -1,9 +1,7 @@
 class TumblrController < ApplicationController
 
   def index
-
-    @tumblr_feed = Feedzirra::Feed.fetch_and_parse(TUMBLR_FEED_URL)
-
+    @feed = SimpleRSS.parse( open("#{TUMBLR_FEED_URL}") )
   end
 
 end
