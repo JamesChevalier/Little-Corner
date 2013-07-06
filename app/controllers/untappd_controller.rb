4799d0e83026fed8cf45d955f997a3c70456cb6c
James Chevalier <jchevalier@gmail.com>
2013-07-06 13:24:35 -0400

    add untappd controller/view

---
+++ app/controllers/untappd_controller.rb
@@ -0,0 +1,6 @@
+class UntappdController < ApplicationController
+  def index
+    @checkins = Untappd::User.feed(UNTAPPD_USER_NAME)
+    @badges   = Untappd::User.badges(UNTAPPD_USER_NAME)
+  end
+end
