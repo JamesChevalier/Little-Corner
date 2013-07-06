891ef56752fd7f0397af177c544972f20c55c1bf
James Chevalier <jchevalier@gmail.com>
2013-07-06 13:24:22 -0400

    add untappd initializer

---
+++ config/initializers/untappd.rb
@@ -0,0 +1,6 @@
+Untappd.configure do |config|
+  config.client_id = UNTAPPD_CLIENT_ID
+  config.client_secret = UNTAPPD_CLIENT_SECRET
+  config.redirect_url = UNTAPPD_OAUTH_REDIRECT_URL
+  config.gmt_offset = UNTAPPD_GMT_OFFSET
+end
