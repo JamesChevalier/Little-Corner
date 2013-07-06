b3365d8eda26c84f94850f39ce679835cc8c392a
James Chevalier <jchevalier@gmail.com>
2013-07-06 13:23:53 -0400

    move twitter config to initializer

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key       = TWITTER_CONSUMER_KEY
+  config.consumer_secret    = TWITTER_CONSUMER_SECRET
+  config.oauth_token        = TWITTER_ACCESS_TOKEN
+  config.oauth_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+end
