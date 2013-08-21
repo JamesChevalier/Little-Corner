f8845fcdb733804df1834b06b243f399f84b120d
James Chevalier <jchevalier@gmail.com>
2013-08-20 20:50:09 -0400

    WIP blog specs

--- spec/controllers/blog_controller_spec.rb
+++ spec/controllers/blog_controller_spec.rb
@@ -12,21 +12,21 @@ describe BlogController do
       it "has a valid @feed" do
         @feed = Feedzirra::Feed.fetch_and_parse(BLOG_FEED_URL)
         get :index, {}
-        assigns(:feed).should eq(@feed) # It can't because each Feedzirra call is hashed differently
+        expect(assigns(:feed).title).to have_content(@feed.title)
       end
     end
 
-    context "without valid configuration" do
-      it "renders index" do
-        get :index
-        response.should be_success
-      end
+    # context "without valid configuration" do
+    #   it "renders index" do
+    #     get :index
+    #     response.should be_success
+    #   end
 
-      it "does not have a valid @feed" do
-        get :index, {}
-        assigns(:feed).should eq(0)
-      end
-    end
+    #   it "does not have a valid @feed" do
+    #     get :index, {}
+    #     assigns(:feed).should eq(0)
+    #   end
+    # end
   end
 
 end
