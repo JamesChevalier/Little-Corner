6c7a64a71ea11c257f92a25672d477dd7cb33ccd
James Chevalier <jchevalier@gmail.com>
2013-09-29 19:50:59 -0400

    Minor Code Formatting

--- spec/controllers/blog_controller_spec.rb
+++ spec/controllers/blog_controller_spec.rb
@@ -2,27 +2,27 @@ require 'spec_helper'
 
 describe BlogController do
 
-  describe "GET index" do
-    context "with valid configuration" do
-      it "renders index" do
+  describe 'GET index' do
+    context 'with valid configuration' do
+      it 'renders index' do
         get :index
         response.should be_success
       end
 
-      it "has a valid @feed" do
+      it 'has a valid @feed' do
         @feed = Feedzirra::Feed.fetch_and_parse(BLOG_FEED_URL)
         get :index, {}
         expect(assigns(:feed).title).to have_content(@feed.title)
       end
     end
 
-    # context "without valid configuration" do
-    #   it "renders index" do
+    # context 'without valid configuration' do
+    #   it 'renders index' do
     #     get :index
     #     response.should be_success
     #   end
 
-    #   it "does not have a valid @feed" do
+    #   it 'does not have a valid @feed' do
     #     get :index, {}
     #     assigns(:feed).should eq(0)
     #   end
