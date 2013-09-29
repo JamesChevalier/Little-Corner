440c69c7c0b1d108d72cd9325cdee4064fc30eaf
James Chevalier <jchevalier@gmail.com>
2013-09-29 19:49:16 -0400

    update contact controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,10 +2,15 @@
 class ContactController < ApplicationController
   def index
     if request.post?
-      m = ContactForm.new(:email => params['contact_form']['email'], :name => params['contact_form']['name'], :message => params['contact_form']['message'])
-      m.valid?
-      m.deliver
-      redirect_to contact_url
+      submission = params['contact_form']
+      @message   = ContactForm.new(email: submission['email'], name: submission['name'], message: submission['message'])
+      if @message.valid?
+        @message.deliver
+        redirect_to root_url, notice: 'Thank you for your message!'
+      else
+        flash[:alert] = 'Something about your message was bad. Did you forget a piece of it?'
+        render :contact
+      end
     else
       @message = ContactForm.new
     end
