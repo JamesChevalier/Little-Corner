0537d6ee489e3c65dce2cad836beeecb854921c4
James Chevalier <jchevalier@gmail.com>
2013-09-29 19:49:42 -0400

    Minor Code Formatting

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -3,14 +3,25 @@ class FoursquareController < ApplicationController
 
   def callback
     if FOURSQUARE_OAUTH_TOKEN.blank?
-      uri                    = URI.parse("https://foursquare.com/oauth2/access_token?client_id=#{FOURSQUARE_CLIENT_ID}&client_secret=#{FOURSQUARE_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=#{FOURSQUARE_CALLBACK_URL}&code=#{params[:code]}")
+      uri                    = URI.parse('https://foursquare.com/oauth2/access_token'\
+                                         "?client_id=#{FOURSQUARE_CLIENT_ID}"\
+                                         "&client_secret=#{FOURSQUARE_CLIENT_SECRET}"\
+                                         '&grant_type=authorization_code'\
+                                         "&redirect_uri=#{FOURSQUARE_CALLBACK_URL}"\
+                                         "&code=#{params[:code]}")
       http                   = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl           = true
       http.verify_mode       = OpenSSL::SSL::VERIFY_NONE
       request                = Net::HTTP::Get.new(uri.request_uri)
       response               = JSON.parse(http.request(request).body)
-      client                 = OAuth2::Client.new(FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET, :site => 'http://foursquare.com/oauth2/', :request_token_path => "/oauth2/request_token", :access_token_path => "/oauth2/access_token", :authorize_path => "/oauth2/authenticate?response_type=code", :parse_json => true)
-      access_token           = OAuth2::AccessToken.new(client, response["access_token"])
+      client                 = OAuth2::Client.new(FOURSQUARE_CLIENT_ID,
+                                                  FOURSQUARE_CLIENT_SECRET,
+                                                  site: 'http://foursquare.com/oauth2/',
+                                                  request_token_path: '/oauth2/request_token',
+                                                  access_token_path: '/oauth2/access_token',
+                                                  authorize_path: '/oauth2/authenticate?response_type=code',
+                                                  parse_json: true)
+      access_token           = OAuth2::AccessToken.new(client, response['access_token'])
       session[:access_token] = access_token.token
     else
       redirect_to foursquare_url
@@ -19,7 +30,10 @@ class FoursquareController < ApplicationController
 
   def connect
     if FOURSQUARE_OAUTH_TOKEN.blank?
-      redirect_to "http://foursquare.com/oauth2/authenticate?client_id=#{FOURSQUARE_CLIENT_ID}&response_type=code&redirect_uri=#{FOURSQUARE_CALLBACK_URL}/"
+      redirect_to 'http://foursquare.com/oauth2/authenticate'\
+                  "?client_id=#{FOURSQUARE_CLIENT_ID}"\
+                  '&response_type=code'\
+                  "&redirect_uri=#{FOURSQUARE_CALLBACK_URL}/"
     else
       redirect_to foursquare_url
     end
--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -3,7 +3,7 @@ class InstagramController < ApplicationController
 
   def callback
     if INSTAGRAM_ACCESS_TOKEN.blank?
-      response               = Instagram.get_access_token(params[:code], :redirect_uri => INSTAGRAM_CALLBACK_URL)
+      response               = Instagram.get_access_token(params[:code], redirect_uri: INSTAGRAM_CALLBACK_URL)
       session[:access_token] = response.access_token
     else
       redirect_to instagram_url
@@ -12,7 +12,7 @@ class InstagramController < ApplicationController
 
   def connect
     if INSTAGRAM_ACCESS_TOKEN.blank?
-      redirect_to Instagram.authorize_url(:redirect_uri => INSTAGRAM_CALLBACK_URL)
+      redirect_to Instagram.authorize_url(redirect_uri: INSTAGRAM_CALLBACK_URL)
     else
       redirect_to instagram_url
     end
