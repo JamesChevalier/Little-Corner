08f45202f2c92e2870dccaaf08b3b22d7b601a39
James Chevalier <jchevalier@gmail.com>
2013-09-29 19:47:45 -0400

    Minor Code Formatting

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,14 +1,12 @@
 # Provides email contact functionality
 class ContactForm < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,   :validate => true
+  attribute :name,    validate: true
+  attribute :email,   validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message, validate: true
 
   def headers
-    {
-      :subject => CONTACT_SUBJECT,
-      :to => CONTACT_TO,
-      :from => %("#{name}" <#{email}>)
-    }
+    { subject: CONTACT_SUBJECT,
+      to: CONTACT_TO,
+      from: %("#{name}" <#{email}>) }
   end
 end
