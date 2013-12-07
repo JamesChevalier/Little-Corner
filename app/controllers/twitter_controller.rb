399bc925e1d4451a5888011227c62f801b5a47e9
James Chevalier <jchevalier@gmail.com>
2013-12-06 21:55:41 -0500

    update Twitter calls to work with new gem

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,10 +2,24 @@
 class TwitterController < ApplicationController
   def index
     @timeline  = Rails.cache.fetch('timeline', expires_in: 1.hour, race_condition_ttl: 10) do
-      Twitter.user_timeline(TWITTER_USER_NAME)
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = TWITTER_CONSUMER_KEY
+        config.consumer_secret     = TWITTER_CONSUMER_SECRET
+        config.access_token        = TWITTER_ACCESS_TOKEN
+        config.access_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+      end
+
+      client.user_timeline(TWITTER_USER_NAME)
     end
     @favorites = Rails.cache.fetch('favorites', expires_in: 1.hour, race_condition_ttl: 10) do
-      Twitter.favorites(TWITTER_USER_NAME)
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = TWITTER_CONSUMER_KEY
+        config.consumer_secret     = TWITTER_CONSUMER_SECRET
+        config.access_token        = TWITTER_ACCESS_TOKEN
+        config.access_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+      end
+
+      client.favorites(TWITTER_USER_NAME)
     end
   end
 end
